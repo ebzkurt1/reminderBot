0dadaffb514d728a65075cd7cd4eb0e6bd7ea8e3
ebzkurt1 <emrebzkurt1@gmail.com>
2024-01-14 14:55:10 +0300

    ADD: proper functionality with commands

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,133 @@
-use teloxide::{prelude::*, utils::command::BotCommands};
+use teloxide::{dispatching::dialogue::InMemStorage, prelude::*};
+
+type MyDialogue = Dialogue<State, InMemStorage<State>>;
+type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
+
+#[derive(Clone, Default)]
+pub enum State {
+    #[default]
+    AllTasks,
+    TodaysTasks,
+    ReceiveTask {
+        task: String,
+    },
+    ReceiveTaskDeadline {
+        task: String,
+        deadline: String,
+    },
+    ReceiveTaskReminder {
+        task: String,
+        deadline: String,
+        reminder: String,
+    },
+}
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    log::info!("Starting command bot...");
+    log::info!("Starting dialogue bot...");
 
     let bot = Bot::from_env();
 
-    Command::repl(bot, answer).await;
+    Dispatcher::builder(
+        bot,
+        Update::filter_message()
+            .enter_dialogue::<Message, InMemStorage<State>, State>()
+            .branch(dptree::case![State::AllTasks].endpoint(all_tasks))
+            .branch(dptree::case![State::TodaysTasks].endpoint(todays_tasks))
+            .branch(dptree::case![State::ReceiveTask { task }].endpoint(receive_task))
+            .branch(dptree::case![State::ReceiveTaskDeadline { task, deadline }].endpoint(receive_task_deadline))
+            .branch(
+                dptree::case![State::ReceiveTaskReminder { task, deadline, reminder }].endpoint(receive_task_reminder),
+            ),
+    )
+    .dependencies(dptree::deps![InMemStorage::<State>::new()])
+    .enable_ctrlc_handler()
+    .build()
+    .dispatch()
+    .await;
 }
 
-#[derive(BotCommands, Clone)]
-#[command(rename_rule = "lowercase", description = "These commands are supported:")]
-enum Command {
-    #[command(description = "display this text.")]
-    Help,
-    #[command(description = "handle a username.")]
-    Username(String),
-    #[command(description = "handle a username and an age.", parse_with = "split")]
-    UsernameAndAge { username: String, age: u8 },
+async fn all_tasks(
+    bot: Bot,
+    dialogue: MyDialogue,
+    message: Message
+) -> HandlerResult {
+    bot.send_message(message.chat_id(), "All tasks").await?;
+    dialogue.exit().await?;
+    Ok(())
 }
 
-async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
-    match cmd {
-        Command::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
-        Command::Username(username) => {
-            bot.send_message(msg.chat.id, format!("Your username is @{username}.")).await?
-        }
-        Command::UsernameAndAge { username, age } => {
-            bot.send_message(msg.chat.id, format!("Your username is @{username} and age is {age}."))
-                .await?
-        }
-    };
+async fn todays_tasks(
+    bot: Bot,
+    dialogue: MyDialogue,
+    message: Message
+) -> HandlerResult {
+    bot.send_message(message.chat_id(), "Today's tasks").await?;
+    dialogue.exit().await?;
+    Ok(())
+}
 
+async fn receive_task(
+    bot: Bot,
+    dialogue: MyDialogue,
+    task: String,
+    message: Message,
+) -> HandlerResult {
+    match message.text() {
+        Some(text) => {
+            bot.send_message(message.chat_id(), "Task received").await?;
+            dialogue.update(State::ReceiveTaskDeadline {
+                task,
+                deadline: text.to_string(),
+            }).await?;
+        }
+        None => {
+            bot.send_message(message.chat_id(), "Please send a text message").await?;
+        }
+    }
+    Ok(())
+}
+
+async fn receive_task_deadline(
+    bot: Bot,
+    dialogue: MyDialogue,
+    task: String,
+    deadline: String,
+    message: Message,
+) -> HandlerResult {
+    match message.text() {
+        Some(text) => {
+            bot.send_message(message.chat_id(), "Task received").await?;
+            dialogue.update(State::ReceiveTaskReminder {
+                task,
+                deadline,
+                reminder: text.to_string(),
+            }).await?;
+        }
+        None => {
+            bot.send_message(message.chat_id(), "Please send a text message").await?;
+        }
+    }
+    Ok(())
+}
+
+async fn receive_task_reminder(
+    bot: Bot,
+    dialogue: MyDialogue,
+    task: String,
+    deadline: String,
+    reminder: String,
+    message: Message,
+) -> HandlerResult {
+    match message.text() {
+        Some(text) => {
+            bot.send_message(message.chat_id(), "Task received").await?;
+            dialogue.exit().await?;
+        }
+        None => {
+            bot.send_message(message.chat_id(), "Please send a text message").await?;
+        }
+    }
     Ok(())
 }
